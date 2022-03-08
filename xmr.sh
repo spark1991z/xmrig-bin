21aa446d16d21c35afd89b5bc1af47540cf42d55
spark1991z <spark1991z@gmail.com>
2022-03-09 02:49:28 +0500

    arm

---
+++ xmr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+POOL=pool.minexmr.com:4444
+WALLET=87UXR53VnD28MjyxcuWQywDMVMx3bRCtgVcMPEHPPfG5fwHhk1y8CySfsAQc3ureBUfsFExBMRokuEpAfKtpt8AU5PzVZoe
+THREADS=1
+#./xmrig-notls -o $POOL -u $WALLET.m3 -p x -t $THREADS --no-huge-pages
