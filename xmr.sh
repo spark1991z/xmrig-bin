a6cc372ba85626469f24141388d10af99b89e286
spark1991z <spark1991z@gmail.com>
2022-03-12 04:58:14 +0500

    XMRig/6.16.4 clang/13.0.1 libuv/1.44.0 OpenSSL/3.0.1

--- xmr.sh
+++ xmr.sh
@@ -3,7 +3,7 @@ POOL=pool.minexmr.com:4444
 WALLET=87UXR53VnD28MjyxcuWQywDMVMx3bRCtgVcMPEHPPfG5fwHhk1y8CySfsAQc3ureBUfsFExBMRokuEpAfKtpt8AU5PzVZoe
 THREADS=2
 CPUAFF=0xC0 #76-- ----
-./xmrig-notls \
+./xmrig \
 -o $POOL \
 -u $WALLET.m3 \
 -p x \
