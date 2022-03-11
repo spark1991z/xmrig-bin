1d448e951d7818f0c21cca8dd3d3edcbf69d3db1
spark1991z <spark1991z@gmail.com>
2022-03-12 03:22:40 +0500

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 POOL=pool.minexmr.com:4444
 WALLET=87UXR53VnD28MjyxcuWQywDMVMx3bRCtgVcMPEHPPfG5fwHhk1y8CySfsAQc3ureBUfsFExBMRokuEpAfKtpt8AU5PzVZoe
-THREADS=1
-#./xmrig-notls -o $POOL -u $WALLET.m3 -p x -t $THREADS --no-huge-pages
+THREADS=2
+CPUAFF=0xC0 #76-- ----
+./xmrig-notls \
+-o $POOL \
+-u $WALLET.m3 \
+-p x \
+-t $THREADS \
+--cpu-affinity $CPUAFF \
+#--no-huge-pages
