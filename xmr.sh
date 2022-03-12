ac8f552044ed493f9bafbe33998d216e4314c057
spark1991z <spark1991z@gmail.com>
2022-03-12 13:06:01 +0500

    Enable huge-pages, switch to 2 threads

--- xmr.sh
+++ xmr.sh
@@ -2,7 +2,7 @@
 POOL=pool.minexmr.com:4444
 WALLET=87UXR53VnD28MjyxcuWQywDMVMx3bRCtgVcMPEHPPfG5fwHhk1y8CySfsAQc3ureBUfsFExBMRokuEpAfKtpt8AU5PzVZoe
 THREADS=2
-CPUAFF=0xC0 #76-- ----
+CPUAFF=0xC0 #7654 3210
 ./xmrig \
 -o $POOL \
 -u $WALLET.m3 \
