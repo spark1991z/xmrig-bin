51263e360910ccf2ce73fbf10407ee051107fe8f
spark1991z <spark1991z>
2022-03-14 10:13:29 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -4,20 +4,20 @@ _arch_=$(uname -m)
 _pf_=unknown
 _rzd_="\n==========================================\n";
 _sudo_=
-_packages_="cmake clang"
+_packages_=
 case $(uname -o) in
 "Android")
- _build_r_=$(getprop ro.build.version.release)
- _build_sdk_=$(getprop ro.build.version.sdk)
- _pf_=android-${_build_sdk_}_${_build_r_}
- _packages_+=" libuv"
+_build_r_=$(getprop ro.build.version.release)
+_build_sdk_=$(getprop ro.build.version.sdk)
+_pf_=android-${_build_sdk_}_${_build_r_}
+_packages_="cmake clang libuv"
 ;;
 "GNU/Linux")
- if [ -f /etc/issue.net ]; then
-  _pf_=$(cat /etc/issue.net | tr "[:upper:]" "[:lower:]" | tr " " "_")
-  _sudo_=sudo
-  _packages_+=" libuv1-dev"
- fi
+if [ -f /etc/issue.net ]; then
+_pf_=$(cat /etc/issue.net | tr "[:upper:]" "[:lower:]" | tr " " "_")
+_sudo_=sudo
+_packages_="cmake clang libuv1-dev libssl-dev"
+fi
 ;;
 esac
 
@@ -25,13 +25,14 @@ echo "${_rzd_}XMRig Build Script (${_pf_}-${_arch_})${_rzd_}"
 read r
 
 echo "-- Step 1. Update & Install packages"
+echo "Packages: ${_packages_}"
 ${_sudo_} apt-get update || return
 ${_sudo_} apt-get install ${_packages_} -y || return
 read r
 
 
 echo "-- Step 2. Cloning repository"
-git clone https://github.com/xmrig/xmrig || return
+git clone https://github.com/xmrig/xmrig
 read r
 
 echo "Step 3. Building"
