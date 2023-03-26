f49394e2fd9bedf805d473a96b8eee29b8c5ab09
spark1991z <spark1991z@gmail.com>
2023-03-27 02:21:51 +0500

    v6.19.1_A3112, update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -22,19 +22,20 @@ if [ ! -f bin/xmrig-${_pf_}-${_arch_} ]; then
 fi
 
 ### Base
-POOL=xmr.2miners.com:2222
+POOL=moscow01.hashvault.pro:443
 WALLET=87UXR53VnD28MjyxcuWQywDMVMx3bRCtgVcMPEHPPfG5fwHhk1y8CySfsAQc3ureBUfsFExBMRokuEpAfKtpt8AU5PzVZoe
-THREADS=1
-CPUAFF=0x01 #7654 3210
+THREADS=6
+CPUAFF=0xfc #7654 3210
 WORKER=${_pf_}-${_arch_}
+PASS_AS_WORKER=1
 
 ### Huge Pages
-NO_HP=1
-USE_HPJ=0
+NO_HP=0
+USE_HPJ=1
 
 ### Random X
-RX_MODE=light # light fast
-USE_RX_1GP=0
+RX_MODE=fast # light fast
+USE_RX_1GP=1
 
 ### Flags
 FLAGS=
@@ -54,5 +55,13 @@ fi
 
 ### XMRig
 echo " * WORKER	${WORKER}"
-bin/xmrig-${_pf_}-${_arch_} -o ${POOL} -u ${WALLET}.${WORKER} -p x -t $THREADS --cpu-affinity $CPUAFF ${FLAGS}
+WALLETWORKER=${WALLET}
+PASSWORKER=x
+if [ $PASS_AS_WORKER -eq 0 ]; then
+ WALLETWORKER+=".${WORKER}"
+else
+ PASSWORKER=${WORKER}
+fi
+
+sudo bin/xmrig-${_pf_}-${_arch_} -o ${POOL} -u ${WALLETWORKER} -p ${PASSWORKER} -t $THREADS --cpu-affinity $CPUAFF ${FLAGS}
 
